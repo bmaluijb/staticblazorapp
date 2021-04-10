bd1af5a8fa4973d0ad74a7d8609903a2ba56e57e
Barry Luijbregts <b.luijbregts@waardedoorit.nl>
2021-04-10 12:50:33 +0000

    added get

--- Api/ProductsPut.cs
+++ Api/ProductsPut.cs
@@ -3,32 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using System.IO;
-using System.Text.Json;
-using Data;
 
 namespace Api
 {
-    public class ProductsPut
+    public class ProductsGet
     {
         private readonly IProductData productData;
 
-        public ProductsPut(IProductData productData)
+        public ProductsGet(IProductData productData)
         {
             this.productData = productData;
         }
 
-        [FunctionName("ProductsPut")]
+        [FunctionName("ProductsGet")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "products")] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "products")] HttpRequest req)
         {
-            var body = await new StreamReader(req.Body).ReadToEndAsync();
-            var product = JsonSerializer.Deserialize<Product>(body, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-
-            var updatedProduct = await productData.UpdateProduct(product);
-            return new OkObjectResult(updatedProduct);
+            var products = await productData.GetProducts();
+            return new OkObjectResult(products);
         }
     }
 }
